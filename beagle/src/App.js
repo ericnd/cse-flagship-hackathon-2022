f8b0f2c85636a5cd8d2715f7849da3b45a0ec785
Eric Do <e.ricdo@outlook.com>
2022-07-17 03:11:57 +1000

    tested new style didn't like it

--- beagle/src/App.js
+++ beagle/src/App.js
@@ -2,37 +2,21 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [dataList, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/data").then (
-      res => res.json()
-    ).then(
-      dataList => {
-        setData(dataList)
-        console.log(dataList)
-      }
-    )
-  }, [])
-
-  const [query, setQuery] = useState("");
-
-  // const {tracks} = Data;
-  // const {items} = tracks;
-  // items looks like this: [{name: 'name1'}, {name: 'name2'}]
-
+  changeSearchTerms => (e) => {
+    this.setState({
+      searchTerms: e.target.value
+    })
+  }
   return (
 
     
     <div className="App">
-      <h1 className='header1'>Beagle 🐶</h1>
-      <h1 className='header1'>Sniffing Out 🐄💩</h1>
-      <div className='wrapper'>
-        <div className='search'>
-        <input type='text' onChange={e => setQuery(e.target.value)} placeholder="Type Something Newsworthy..." />
-
-        </div>
-      </div>
+      <h1> Beagle Search </h1>
+      <form action = "">
+        <input type="text" value=''onChange={this.changeSearchTerms} placeholder='search news' />
+        <button type= 'submit' onClick = {this.changeSearchTerms}></button>
+      </form>
+      {/* {searchResults} */}
     </div>
   );
 }
